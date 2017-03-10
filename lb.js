3d0deab5f96565a0e08a31dfc6eb7b5c413d292b
Ashok Kumar <brownsmrt@gmail.com>
2017-03-10 22:51:20 +0530

    Create lb.js

---
+++ lb.js
@@ -0,0 +1,26 @@
+<style>
+#fvDtJoYiKpAe {
+display: none;
+margin-bottom: 30px;
+padding: 20px 10px;
+background: #D30000;
+text-align: center;
+font-weight: bold;
+color: #fff;
+border-radius: 5px;
+}
+</style>
+
+<div id="fvDtJoYiKpAe">
+  Our website is made possible by displaying online advertisements to our visitors.<br>
+  Please consider supporting us by disabling your ad blocker.
+</div>
+
+<script src="/ads.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+if(!document.getElementById('zgrUOdoSKect')){
+  document.getElementById('fvDtJoYiKpAe').style.display='block';
+}
+
+</script>
