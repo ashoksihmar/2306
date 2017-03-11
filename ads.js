3a6ecf94ecc16bc74bb1a1b4c9ee300c8a2cb135
Ashok Kumar <brownsmrt@gmail.com>
2017-03-11 14:39:54 +0530

    time.js

--- ads.js
+++ ads.js
@@ -1,1 +1,20 @@
-isAdBlockActive=false;
+<script>
+// scripts here:
+window.onload = function submitQuiz(){
+//Code for Timer	
+		var seconds_left = 30;
+var interval = setInterval(function() {
+    document.getElementById('timer_div').innerHTML = "00:"+ (--seconds_left);
+
+    if (seconds_left <= 0)
+    {
+        clearInterval(interval);
+        document.getElementById('timer_div').innerHTML = 'Yours TimeOut...';
+    }
+}, 1000);
+
+
+		setTimeout(function(){document.getElementById('submitButton').click(); }, 3000);
+ 
+};
+</script>
